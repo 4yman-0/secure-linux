e6ffe5dbbf64f7116109db6d41ea0e0781b2f37b
4yman <170770027+4yman-0@users.noreply.github.com>
2024-09-01 20:40:43 +0100

    Check for Errors and run script w/ root
    Check if `ufw` and `sysctl` are in `/usr/sbin`
    Use a `#!` trick I made to run script as UID 0.

--- secure.sh
+++ secure.sh
@@ -1,35 +1,43 @@
-#!/bin/bash
+#!/bin/sudo bash
 #-----------------------
 #--Required Packages-
 #-ufw
 #-fail2ban
 
 # --- Setup UFW rules
-sudo ufw limit 22/tcp  
-sudo ufw allow 80/tcp  
-sudo ufw allow 443/tcp  
-sudo ufw default deny incoming  
-sudo ufw default allow outgoing
-sudo ufw enable
+if [[ -f /usr/sbin/ufw ]]; then
+    ufw limit 22/tcp
+    ufw allow 80/tcp  
+    ufw allow 443/tcp  
+    ufw default deny incoming  
+    ufw default allow outgoing
+    ufw enable
+else
+    echo "UFW not found"
+fi
 
 # --- Harden /etc/sysctl.conf
-sudo sysctl kernel.modules_disabled=1
-sudo sysctl -a
-sudo sysctl -A
-sudo sysctl mib
-sudo sysctl net.ipv4.conf.all.rp_filter
-sudo sysctl -a --pattern 'net.ipv4.conf.(eth|wlan)0.arp'
+if [[ -f /usr/sbin/sysctl ]]; then
+    sysctl kernel.modules_disabled=1
+    sysctl -a
+    sysctl -A
+    sysctl mib
+    sysctl net.ipv4.conf.all.rp_filter
+    sysctl -a --pattern 'net.ipv4.conf.(eth|wlan)0.arp'
+else
+    echo "sysctl not found"
+fi
 
 # --- PREVENT IP SPOOFS
-cat <<EOF > /etc/host.conf
+cat << EOF > /etc/host.conf
 order bind,hosts
 multi on
 EOF
 
 # --- Enable fail2ban
-sudo cp jail.local /etc/fail2ban/
-sudo systemctl enable fail2ban
-sudo systemctl start fail2ban
+cp jail.local /etc/fail2ban/
+systemctl enable fail2ban
+systemctl start fail2ban
 
 echo "listening ports"
-sudo netstat -tunlp
+netstat -tunlp
