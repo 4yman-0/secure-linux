8239802d83ab413bd9617b0fdcca887f5f46d9d7
4yman <170770027+4yman-0@users.noreply.github.com>
2024-10-22 12:44:20 +0100

    use spaces

--- secure.sh
+++ secure.sh
@@ -5,10 +5,10 @@
 #-fail2ban
 
 # --- Setup UFW rules
-sudo ufw limit 22/tcp  
-sudo ufw allow 80/tcp  
-sudo ufw allow 443/tcp  
-sudo ufw default deny incoming  
+sudo ufw limit 22/tcp
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw enable
 
